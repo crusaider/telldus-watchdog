1fdeb8027dff7583ecec2e8b653556e9632604f4
Jonas Andreasson <jonas@idfyed.com>
2020-12-13 23:59:25 +0100

    Update deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,29 @@
 'use strict';
-var path = require('path');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
-gulp.task('static', function () {
+function staticLint() {
   return gulp.src('**/*.js')
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
+}
 
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
-
-gulp.task('pre-test', function () {
+function preTest() {
   return gulp.src('lib/**/*.js')
     .pipe(excludeGitignore())
     .pipe(istanbul({
       includeUntested: true
     }))
     .pipe(istanbul.hookRequire());
-});
+}
 
-gulp.task('test', ['pre-test'], function (cb) {
+function doTest(cb) {
   var mochaErr;
 
   gulp.src('test/**/*.js')
@@ -42,11 +36,15 @@ gulp.task('test', ['pre-test'], function (cb) {
     .on('end', function () {
       cb(mochaErr);
     });
-});
+}
 
-gulp.task('watch', function () {
-  gulp.watch(['lib/**/*.js', 'test/**'], ['test']);
-});
+const test = gulp.series(preTest,doTest);
 
-gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test']);
+function watch() {
+  gulp.watch(['lib/**/*.js', 'test/**'], test);
+}
+
+
+exports.watch = watch;
+exports.prepublishOnly = gulp.series(staticLint, test);
+exports.default = gulp.series(staticLint, test);
